a0ff8c01dbbcef138acc2ecbc47f4d2331886463
winwab1 <Brian.Winward@investorsgroup.com>
2020-03-05 12:55:58 -0500

    Firebase authentication

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,6 +5,8 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
+
 class SignIn extends Component {
     constructor(props) {
         super(props);
@@ -15,10 +17,17 @@ class SignIn extends Component {
         };
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state;
+
+        try {
+          await auth.signInWithEmailAndPassword(email, password);
+          this.setState({ email: '', password: '' });
+        } catch(error) {
+          throw new Error(error)
+        }
     }
 
     handleChange = event => {
@@ -49,7 +58,10 @@ class SignIn extends Component {
                 label="Password"
                 required
               />
-              <CustomButton type="submit">Sign in</CustomButton>
+              <div className='buttons'>
+                <CustomButton type="submit">Sign in</CustomButton>
+                <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+              </div>
             </form>
           </div>
         );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,54 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBa-CQ2Jn8lBN2E4ZSJmR1EaC8o5Pxz3YI",
+    authDomain: "crowndb-3ea13.firebaseapp.com",
+    databaseURL: "https://crowndb-3ea13.firebaseio.com",
+    projectId: "crowndb-3ea13",
+    storageBucket: "crowndb-3ea13.appspot.com",
+    messagingSenderId: "888161984593",
+    appId: "1:888161984593:web:6fb4b7e26e8210deba5870",
+    measurementId: "G-30FSKNSE5S"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapshot = await userRef.get();
+
+    // does user exist?
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        // if it doesn't exist create a new user
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch(error) {
+            throw new Error('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+// Google authentication
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
